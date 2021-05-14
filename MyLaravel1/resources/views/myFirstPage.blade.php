a7e0b7f5cd305dd20581a3e673c8bd614525b85b
Natale Capristo <andreacapristo1@gmail.com>
2021-05-14 17:00:55 +0200

    Laravel first steps

---
+++ MyLaravel1/resources/views/myFirstPage.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My First Time with Laravel</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+  </head>
+  <body>
+    <div class="container">
+
+      <h1>Hello There</h1>
+
+      <p>
+        This is my first time using Laravel's framework.
+        <h3>
+          I'm so excited...
+        </h3>
+      </p>
+
+    </div>
+  </body>
+</html>
